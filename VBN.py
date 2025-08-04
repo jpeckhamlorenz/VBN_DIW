f11e11dcc85bd31a4310a862e2a0b407e32e10f4
James Lorenz <jplorenz@umich.edu>
2025-08-04 16:19:08 -0400

    added POLI=1 and DLAY=1 commands
    Command the linear actuators to have minimal delays when they receive a movement command

--- VBN.py
+++ VBN.py
@@ -44,6 +44,8 @@ def func01():
     stage1.setSpeed(start_speed)
     stage1.setUnits(Units.mu)
     stage1.setDPOS(-15000)
+    stage1.sendCommand("POLI=1")
+    stage1.sendCommand("DLAY=1")
     sleep(2)
 
 def func02():
@@ -52,6 +54,8 @@ def func02():
     stage2.setSpeed(start_speed)
     stage2.setUnits(Units.mu)
     stage2.setDPOS(-15000)
+    stage2.sendCommand("POLI=1")
+    stage2.sendCommand("DLAY=1")
     sleep(2)
     
 def func1(position, speed):
